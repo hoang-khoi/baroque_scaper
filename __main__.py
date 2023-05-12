86e12050c06b2b1ff0f9dbe2b1e332c74c952a0b
Khoi HOANG <khoi_hoang@hotmail.com>
2023-05-12 21:52:22 +0900

    Catch everything, just in case

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ __license__ = "MIT"
 
 import pathlib
 import sys
+import traceback
 
 from baroque_scaper.baroque_scraper_impl import BaroqueScaperImpl
 
@@ -23,5 +24,5 @@ if __name__ == "__main__":
     """ This is executed when run from the command line """
     try:
         main()
-    except Exception as e:
-        print("EXCEPTION " + e)
+    except Exception:
+        traceback.print_exc()
